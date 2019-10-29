b4cfd5c17a0df495213c715034791b1dc74d2a94
Vitordan5 <vitordanirl@gmail.com>
2019-10-29 19:26:36 -0300

    Scraping do rotten

--- CODIGO/ROTTEN/SCRAP.py
+++ CODIGO/ROTTEN/SCRAP.py
@@ -1,23 +1,36 @@
 import requests
-results = requests.get('https://www.rottentomatoes.com/browse/tv-list-2/')
-from bs4 import BeautifulSoup
-title = 'a'
-notes = 'b'
-year = 'c'
-with open('rotten.csv', 'w') as s:
-    s.write('SERIES;NOTAS\n')
+import json
+from enum import Enum
 
-def scrap ():
-    soup = BeautifulSoup(results.text, 'html.parser')
 
-    title = soup.findAll('div', class_='movie_info')
-    title = BeautifulSoup(str(title), 'html.parser').findAll('h3')
-    notes = soup.findAll('div', class_='movie_info')
-    notes = BeautifulSoup(str(notes), 'html.parser').findAll('span',class_='tMeterScore')
+class TvBrowsingCategory(Enum):
+    new_tv_tonight = "tv-list-1"
+    most_popular = "tv-list-2"
+    certified_fresh = "tv-list-3"
 
-    with open('rotten.csv', 'a') as s:
-        i = 0
-        for i in range(0, len(title)):
-            s.write(title[i].text.replace(',', '').replace(';', '') + ';' + notes[i].text + ';' + '\n')
-scrap()
-print(title)
+
+class RottenTomatoesClient:
+    BASE_URL = "https://www.rottentomatoes.com/api/private"
+    BASE_V1_URL = "{base_url}/v1.0".format(base_url=BASE_URL)
+    BASE_V2_URL = "{base_url}/v2.0".format(base_url=BASE_URL)
+    MOVIE_DETAILS_URL = "{base_url}/movies".format(base_url=BASE_V1_URL)
+    SEARCH_URL = "{base_url}/search".format(base_url=BASE_V2_URL)
+    BROWSE_URL = "{base_url}/browse".format(base_url=BASE_V2_URL)
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def browse_tv_shows(category=TvBrowsingCategory.most_popular):
+        r = requests.get(url=RottenTomatoesClient.BROWSE_URL, params={"type": category.value})
+
+        r.raise_for_status()
+
+        return r.json()
+
+
+result = RottenTomatoesClient.browse_tv_shows()
+data = json.dumps(result , indent=2)
+
+for item in result["results"]:
+    print(item['title'], item['tomatoScore'])
