a44743f4d5bd24d7c8fe119f6b076fdad600829e
ferpsalles <fee_salles@hotmail.com>
2019-11-10 19:54:40 -0300

    Teste com flask

---
+++ CODIGO/TESTES/Flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+def scrapping():
+    rotten = requests.get("https://www.rottentomatoes.com/browse/tv-list-2/")
+    soup = BeautifulSoup(rotten.text, 'html.parser')
+    data = soup.findAll('td', class_="middle_col")
+    response = [item.get_text() for item in data]
+
+    return response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    data = scrapping()
+
+    return jsonify({'response': data})
+
+
+app.run()
